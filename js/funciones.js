908117d88f6525f1565569af7b9bbebe74603b9c
Monica Rossi <monicarossi3@hotmail.com>
2023-02-02 04:39:30 -0300

    Cambios de estilos

--- js/funciones.js
+++ js/funciones.js
@@ -235,7 +235,37 @@ $(document).ready(function(){
     }
   })
 });
+$('#next-img').click(function (){
+  $('#show-img').attr('src', $(".show-small-img[alt='now']").next().attr('src'))
+  $('#big-img').attr('src', $(".show-small-img[alt='now']").next().attr('src'))
+  $(".show-small-img[alt='now']").next().css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
+  $(".show-small-img[alt='now']").next().attr('alt', 'now').siblings().removeAttr('alt')
+  if ($('#small-img-roll').children().length > 4) {
+    if ($(".show-small-img[alt='now']").index() >= 3 && $(".show-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
+      $('#small-img-roll').css('left', -($(".show-small-img[alt='now']").index() - 2) * 76 + 'px')
+    } else if ($(".show-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
+      $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
+    } else {
+      $('#small-img-roll').css('left', '0')
+    }
+  }
+})
 
+$('#prev-img').click(function (){
+  $('#show-img').attr('src', $(".show-small-img[alt='now']").prev().attr('src'))
+  $('#big-img').attr('src', $(".show-small-img[alt='now']").prev().attr('src'))
+  $(".show-small-img[alt='now']").prev().css({'border': 'solid 1px #951b25', 'padding': '2px'}).siblings().css({'border': 'none', 'padding': '0'})
+  $(".show-small-img[alt='now']").prev().attr('alt', 'now').siblings().removeAttr('alt')
+  if ($('#small-img-roll').children().length > 4) {
+    if ($(".show-small-img[alt='now']").index() >= 3 && $(".show-small-img[alt='now']").index() < $('#small-img-roll').children().length - 1){
+      $('#small-img-roll').css('left', -($(".show-small-img[alt='now']").index() - 2) * 76 + 'px')
+    } else if ($(".show-small-img[alt='now']").index() == $('#small-img-roll').children().length - 1) {
+      $('#small-img-roll').css('left', -($('#small-img-roll').children().length - 4) * 76 + 'px')
+    } else {
+      $('#small-img-roll').css('left', '0')
+    }
+  }
+})
 //EFECTO ZOOM PRODUCTO CATALOGO
 
 ;(function($) {
